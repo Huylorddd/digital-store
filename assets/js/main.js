b6ba7386dc9dbfada66b90f97abe789e4c285833
itechserver <imhyugii@gmail.com>
2026-05-04 09:50:03 +0700

    Added temporary +84 tel and citizen ID regexes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,6 +44,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/; 
     const nameRegex = /^(\p{Lu}\p{Ll}*\s?)+$/u;     // Lu : chữ hoa unicode, Ll: chữ thường unicod, Unicode hỗ trợ tên Việt
     const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/; 
+    //const phoneRegex = /^(0|\\+84)[3|5|7|8|9][0-9]{8}$/;
+    //const cccdRegex = /^[0-9]{12}$/;
 
     // Xử lý Đăng nhập
     const btnLogin = document.getElementById('btnLogin');
